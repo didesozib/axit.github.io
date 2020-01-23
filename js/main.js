41175a73c1c982a190aaae95ad73b88da1c1f846
Dider Hossain Sozib <45062869+didesozib@users.noreply.github.com>
2020-01-23 12:44:17 +0600

    Fast website

---
+++ js/main.js
@@ -0,0 +1,42 @@
+// Tab Section Functionality
+$(document).ready(function(){
+
+	$('.tab-one').addClass('active');
+	$('.one-content').show();
+	$('.two-content').hide();
+	$('.three-content').hide();
+
+	$('.tab-one').click(function(){
+		$('.one-content').show();
+		$('.two-content').hide();
+		$('.three-content').hide();
+
+		// Add Class Active
+		$('.tab-one').addClass('active');
+		$('.tab-two').removeClass('active');
+		$('.tab-three').removeClass('active');
+	});
+
+	$('.tab-two').click(function(){
+		$('.one-content').hide();
+		$('.two-content').show();
+		$('.three-content').hide();
+
+		// Add Class Active
+		$('.tab-one').removeClass('active');
+		$('.tab-two').addClass('active');
+		$('.tab-three').removeClass('active');
+	});
+
+	$('.tab-three').click(function(){
+		$('.one-content').hide();
+		$('.two-content').hide();
+		$('.three-content').show();
+
+		// Add Class Active
+		$('.tab-one').removeClass('active');
+		$('.tab-two').removeClass('active');
+		$('.tab-three').addClass('active');
+	});
+
+});
